0013176619cd92d3f1051724e690d36f82465b06
Ben Groot <bengroot12@hotmail.com>
2020-01-06 15:11:53 +0100

    first simplistic version

--- models.py
+++ models.py
@@ -30,19 +30,26 @@ class House(object):
 
             # Checks capacity and if available checks distance to battery
             if battery.capacity > self.output:
-                distance = (self.coordinates[0] + self.coordinates[1]) - (battery.coordinates[0] - battery.coordinates[1])
-                distanceList.add(distance)
+                distance = abs(self.coordinates[0] - battery.coordinates[0]) + abs(self.coordinates[1] - battery.coordinates[1])
+                print(f"Self = {self.coordinates}. Battery = {battery.coordinates}")
+
+                distanceList.append(distance)
             else:
-                distanceList.add(None)
+                distanceList.append(None)
 
         # Finds nearest available battery
-        shortestDistance = min(distanceList)
-        shortestIndex = batteries.index(shortestDistance)
+        print(f"{distanceList}, hey")
+        shortestDistance = min(i for i in distanceList if i is not None)
+        print(shortestDistance)
+        shortestIndex = distanceList.index(shortestDistance)
 
         # Sets battery and route
-        self.battery = batteries(shortestIndex).coordinates
+        self.battery = batteries[shortestIndex].coordinates
         self.route = (self.coordinates[0], self.battery[1])
         battery.capacity -= self.output
+
+    def __str__(self):
+        return f"{self.coordinates}, {self.output}, {self.route}, {self.battery}\n"
 
 class Battery(object):
     """Het model voor de batterij."""
@@ -52,3 +59,6 @@ class Battery(object):
 
         self.coordinates = (x, y)
         self.capacity = capacity
+
+    def __str__(self):
+        return f"Battery: {self.coordinates}"
---
+++ test.py
@@ -0,0 +1,17 @@
+"""
+06-01-2020
+
+
+Test document. Vergeet niet te verwijderen!
+
+The Group Formerly Known as 'The Prince Statement'
+Ben Groot, Boy Stekelbos, Momo Schaap
+"""
+def subtract(tup1, tup2):
+    return (tup1[0] - tup2[0], tup1[1] - tup2[1])
+
+test1 = (5,5)
+test2 = (9, 9)
+
+result = subtract(test1, test2)
+print(result)
