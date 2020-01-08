ea42eb60d5d4fd9e7523137feaddc4fc939f8651
Mo Schaap <moschaap@wcw-stud-145-109-0-64.wireless.uva.nl>
2020-01-08 12:49:48 +0100

    Visualization (compatible with old version)

--- visualization.py
+++ visualization.py
@@ -4,88 +4,73 @@ import csv
 
 def load():
 
-    allNetworks = []
-    connection = []
+    # allNetworks = []
     
+    
+    allCoordinates = []
 
-    with open(f"../SmartGrid/paths.txt", "r") as f:
+    with open(f"paths.txt", "r") as f:
 
         next(f)
 
-        for line in f:
-            allCoordinates = line.split(',')
-            networks.append(allCoordinates)
-
-
-
-    print(networks)
-
-            
-
-
-
-
-def main():
-
-    # with open(f"paths.txt", "r") as f:
         
 
-
-    # x = np.arange(1,5)
-    # y = x**3
+        for line in f:
 
 
-    # plt.plot([1,2,3,4],[1,4,9,16],'go-', x, y,'r^-')
-    # plt.axis([0, 6, 0, 20])
-    # plt.title("First plot")
-    # plt.xlabel("Xlabel")
-    # plt.ylabel("Ylabel")
-    # plt.grid()
-    # plt.show()
+            connection = []
+            connectionCoordinates = []
+
+            for element in line.split(','):
+                    
+                element = element.strip('" () "')
+                connection.append(float(element))
+                
+            connectionCoordinates = [connection[0], connection[1], connection[3], connection[4], connection[5], connection[6]]
+            allCoordinates.append(connectionCoordinates)
+
+    return allCoordinates
 
 
-    x = [2,2,1]
-    y = [5,7,7]
-    x2 = [5,9,9]
-    y2 = [2,2,4]
-    x3 = [6,6,9]
-    y3 = [1,5,5]
-    x4 = [5,6,6]
-    y4 = [2,2,8]
+def visualize():
 
-    xlist = [x, x2, x3, x4]
-    ylist = [y, y2, y3, y4]
-    args = ['go-', 'k^-', 'bo-']
-    lines = ['r--', 'b.-', 'c:', 'm-']
+    allCoordinates = load()
 
+    xlist = []
+    ylist = []
+    lines = ['r:', 'b-.', 'c:', 'm-.']
 
-    for i in range(4):
+    for connection in allCoordinates:
+
+        x = [connection[0], connection[2], connection[4]]
+        xlist.append(x)
+
+        y = [connection[1], connection[3], connection[5]]
+        ylist.append(y)
+
+    for i in range(len(allCoordinates)):
 
         # Lines
-        plt.plot(xlist[i], ylist[i], lines[i])
+        plt.plot(xlist[i], ylist[i], lines[i % 3])
         
-        # Houses
-        plt.plot(xlist[i][0], ylist[i][0], 'bs')
-
         # Batteries
-        plt.plot(xlist[i][2], ylist[i][2], 'r^')
+        plt.plot(xlist[i][0], ylist[i][0], 'r^')
 
+        # Houses
+        plt.plot(xlist[i][2], ylist[i][2], 'bs')
 
-    # for i in range(3):
-    #     plt.plot(xlist[i], ylist[i], args[i])
-
-    # plt.plot(x, y, 'go-')
-    # plt.plot(x2, y2, 'k^-')
-    # plt.plot(x3, y3, 'bo-')
-    
-
-    plt.axis([0, 10, 0, 10])
+    ticks = np.arange(0,50,1)
+    plt.axis([0, 50, 0, 50])
+    plt.xticks(ticks)
+    plt.yticks(ticks)
     plt.title("First plot")
     plt.xlabel("Xlabel")
     plt.ylabel("Ylabel")
-    plt.grid()
+    plt.grid(True)
     plt.show()
 
+
+def main():
 
 
     """https://stackoverflow.com/questions/35363444/plotting-lines-connecting-points"""
@@ -96,4 +81,4 @@ def main():
         plt.plot([x1,x2],[y1,y2],'k-')
 
 
-load()
+visualize()
