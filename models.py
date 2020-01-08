1edc151e8f8fce912254ed3212383f0750a1c979
btstekelbos <bt.stekelbos@hotmail.com>
2020-01-08 13:03:15 +0100

    Working on grid search

--- models.py
+++ models.py
@@ -7,6 +7,25 @@ De modellen voor de SmartGrid assignment.
 The Group Formerly Known as 'The Prince Statement'
 Ben Groot, Boy Stekelbos, Momo Schaap
 """
+class Network(object):
+    """The model for a cable network."""
+
+    def __init__(self, x, y, capacity):
+        """Initializes the source location and capacity of the battery."""
+
+        self.capacity = capacity
+        self.source = (x, y)
+        
+        # Verandert naar een dictionarry van lijsten
+        self.cables = {self.source :[]}
+        self.cables = {1:[], 2:[], 3:[], 4:[], 5:[]}
+
+
+        sourceCable = (x, y)
+        self.cables.append(sourceCable)
+
+    def __str__(self):
+        return f"{self.capacity}, {self.source}, cables: {self.cables}"
 
 class House(object):
     """Het model voor het huis object."""
@@ -23,30 +42,56 @@ class House(object):
     def BatteryCheck(self, batteries):
         """Checks the best battery based on distance and current capacity."""
 
-        # Makes empty list to store distance between house and battery
-        distanceList = []
+    # https://stackoverflow.com/questions/398299/looping-in-a-spiral
+        #  maak een figuratieve (0,0) positie voor het huis.
+        X = self.coordinates[0]
+        Y = self.coordinates[1]
+
+        x = y = 0
+
+        #  Search functie begint bij (0,-1)
+        dx = 0
+        dy = -1
+
+        for i in range(max(X, Y)**2):
+            
+            # Loopt over een grid van 4 bij 4 
+            if (-X/2 < x <= X/2) and (-Y/2 < y <= Y/2):
+               
+                print (x, y)
+                # Objectieve positie berekenen
+                ObjectiveX: abs(self.coordinates[0] - x)
+                ObjectiveY: abs(self.coordinates[1] - y)
+                ObjectivePosition = (X,Y)
 
 
-        for battery in batteries:
+                for i in Network.cables.keys():
+                 
+                    if ObjectivePosition in Network.cables[i]:
+                        if Network.capacity > self.output:
 
-            # Checks capacity and if available checks distance to battery
-            if battery.capacity > self.output:
-                distance = abs(self.coordinates[0] - battery.coordinates[0]) + abs(self.coordinates[1] - battery.coordinates[1])
-                print(f"Self = {self.coordinates}. Battery = {battery.coordinates}, Capacity = {battery.capacity}")
 
-                distanceList.append(distance)
-            else:
-                distanceList.append(None)
 
-        # Finds nearest available battery
-        shortestDistance = min(i for i in distanceList if i is not None)
-        shortestIndex = distanceList.index(shortestDistance)
+
+
+
+                # Over de lijst van bekende kabels loopen, if (Objectieve positie) in self.cables:
+                if ObjectivePosition in self.cables
+
+                # Als het capaciteit heeft
+
+            if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+                dx, dy = -dy, dx
+            x, y = x+dx, y+dy              
+
 
         # Sets battery and route
         self.battery = batteries[shortestIndex].coordinates
         self.route = (self.coordinates[0], self.battery[1])
         self.cost = shortestDistance * 9
         battery.capacity -= self.output
+
+        
 
     def __str__(self):
         return f"{self.coordinates}, {self.output}, {self.route}, {self.battery}, {self.cost}\n"
@@ -63,36 +108,7 @@ class Battery(object):
     def __str__(self):
         return f"Battery: {self.coordinates}"
 
-class Grid(object):
-    """The model for treating cables and houses the same as a battery """
-
-    def __init__(self, x, y, output):
-        """Initializeert het huis."""
-
-        self.coordinates = (x, y)
-        self.output = output
-        self.route = (None, None)
-        self.battery = (None, None)
-        self.cost = 0
 
 
-    def Network(self):
-        self.network = []
-        
 
-    def Battery(self, x, y, capacity):
-        self.coordinates = (x,y)
-        self.capacity = capacity
-
-    #  Lijst van tuples dat uiteindelijk Alle coordinaten van kabels die op 1 specifiek batterij geconnect zijn. zodat er op deze coordinaten
-    #  Gecheckt kan worden voor afstand calculatie
-    
-
-
-    # Batterij coordinaten zodat er een initiele afstand berekening gedaan kan worden.
-
-    # Huis coordinaten die connected zijn met de berekende dichtstbijzijnde batterij en dus lid zijn van het netwerk waarop we willen connecten. Opgeslagen in een lijst van tuples.
-    self.houses = []
-
-    #  Vanaf de batterij berekenen naar welk huis eerst kabels gelegd wordt, prioriteit geven aan low output huizen.
-    
+   
